c2240097e01a555b6e454924d64e851bbd9b924b
Sharvilak Thakore <sharvilakthakore@yahoo.com>
2017-03-15 22:46:19 +0530

    favicon.ico error removed

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.engine('.hbs',expresshbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
